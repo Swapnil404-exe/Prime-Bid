26ec1ebc3e0168d3c1ab932674245f72bb58fc39
Swapnil404-exe <swapnildhamija53@gmail.com>
2025-02-13 14:43:27 +0530

    pushing to server

---
+++ backend/database/connection.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+export const connection = () => {
+  mongoose
+    .connect(process.env.MONGO_URI, {
+      dbName: process.env.DB_NAME,
+    })
+    .then(() => {
+      console.log("Connected to database.");
+    })
+    .catch((err) => {
+      console.log(`Some error occured while connecting to database: ${err}`);
+    });
+};
